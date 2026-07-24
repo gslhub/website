e22677674028b5244fe5ca93fed346346650216a
Edu <133777880+Emmakex@users.noreply.github.com>
2026-07-24 13:27:57 +0200

    feat(admin): add branded GSLHub admin icon

---
+++ components/admin/GSLHubAdminIcon.tsx
@@ -0,0 +1,9 @@
+import { GSLHubIcon } from '../brand/GSLHubLogo';
+
+export default function GSLHubAdminIcon() {
+  return (
+    <div style={{ width: '42px' }}>
+      <GSLHubIcon className="gslhub-admin-brand-icon" />
+    </div>
+  );
+}
