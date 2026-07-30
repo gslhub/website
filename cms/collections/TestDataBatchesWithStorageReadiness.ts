f0d7fe24b8c85b25ee8196c94847389c9974eb32
Edu <133777880+Emmakex@users.noreply.github.com>
2026-07-30 07:33:26 +0200

    feat(pilot): use verified readiness endpoint

--- cms/collections/TestDataBatchesWithStorageReadiness.ts
+++ cms/collections/TestDataBatchesWithStorageReadiness.ts
@@ -1,14 +1,14 @@
 import type { CollectionConfig } from 'payload';
 
-import { getPilotReadinessEndpoint } from '../endpoints/getPilotReadiness';
 import { getStorageReadinessEndpoint } from '../endpoints/getStorageReadiness';
+import { getVerifiedPilotReadinessEndpoint } from '../endpoints/getVerifiedPilotReadiness';
 import { TestDataBatches as BaseTestDataBatches } from './TestDataBatches';
 
 export const TestDataBatches: CollectionConfig = {
   ...BaseTestDataBatches,
   endpoints: [
     getStorageReadinessEndpoint,
-    getPilotReadinessEndpoint,
+    getVerifiedPilotReadinessEndpoint,
     ...(BaseTestDataBatches.endpoints || []),
   ],
 };
